0018237ed683c84bd7a34b019e0e1dd550960f16
lllllLeo <andwithyj@gmail.com>
2019-03-04 03:02:24 +0900

    cookie, content-type

--- Java/web-application-server/README.md
+++ Java/web-application-server/README.md
@@ -1,12 +1,12 @@
-# 실습을 위한 개발 환경 세팅
-* https://github.com/slipp/web-application-server 프로젝트를 자신의 계정으로 Fork한다. Github 우측 상단의 Fork 버튼을 클릭하면 자신의 계정으로 Fork된다.
-* Fork한 프로젝트를 eclipse 또는 터미널에서 clone 한다.
-* Fork한 프로젝트를 eclipse로 import한 후에 Maven 빌드 도구를 활용해 eclipse 프로젝트로 변환한다.(mvn eclipse:clean eclipse:eclipse)
-* 빌드가 성공하면 반드시 refresh(fn + f5)를 실행해야 한다.
-
 # 웹 서버 시작 및 테스트
-* webserver.WebServer 는 사용자의 요청을 받아 RequestHandler에 작업을 위임하는 클래스이다.
-* 사용자 요청에 대한 모든 처리는 RequestHandler 클래스의 run() 메서드가 담당한다.
+* WebServer
+   * 웹 서버를 시작하고, 사용자의 요청이 있을 때까지 대기 상태에 있다가 사용자의 요청이 있을 경우 받아서 RequestHandler에 작업을 위임하는 클래스이다.
+   * ServerSocket에 사용자 요청이 발생하는 순간 클라이언트와 연결을 담당하는 Socket을 RequestHandler에 전달하면서 새로운 스레드를 실행하는 방식으로 멀티스레드 프로그래밍을 지원하고 있다.
+* ServerSocket
+   * Java에 포함되어 있는 클래스로 사용자 요청이 발생할 때까지 대기 상태에 있도록 지원하는 클래스
+* RequestHandler
+   * Thread를 상속하고 있으며, 사용자의 요청에 대한 처리와 응답에 대한 처리를 담당(<code>run()</code> 메서드가 담당)하는 클래스이다.
+
 * WebServer를 실행한 후 브라우저에서 http://localhost:8080으로 접속해 "Hello World" 메시지가 출력되는지 확인한다.
 
 # 각 요구사항별 학습 내용 정리
@@ -26,6 +26,18 @@
   안에 있는 <code>InputStream</code>과 <code>OutputStream</code>이 try의 body 절이 끝나면 자동으로 자원을 <code>close()</code> 한다. 
   이게 가능한 이유는 <code>InputStream</code>과 <code>OutputStream</code>은 <code>Closeable</code>를 Implements를 하고있기 때문이다. 
   이외에도 이와 같이 자동으로 <code>close()</code>를 하고 싶으면 이 각각의 클래스 파일들이 <code>Closeable</code> 인터페이스를 implements 하고 있을때 이와 동일하게 사용할 수 있다.
+  
+* BufferedReader
+  * readLine() : 값을 읽어올 때, String 값으로 개행문자를 포함해 한 줄을 전부 읽어온다. (Scanner메서드에서 nextLine()과 비슷)
+  * read() : 값을 읽어올 때, int 값으로 변형하여 읽어온다. 
+    * 예를들면
+    * ~.txt에 1이라는 숫자가 저장되어 있는데 read()로 읽으면 int 형 숫자 1로 읽어오는 것이 아니라 txt형식으로 저장된 ASCII 형식의 문자값 '1'을 읽어오는 것이므로 int 값으로 49를 읽어온다.
+    * 해결방법
+      1. int a = br.read() - 48;  
+       br.readLine();
+       > ASCII 값에서 48을 뺀 뒤 엔터 값을 읽어온다.
+      1. int a = integer.parseInt(br.readLine());
+       > 엔터 값을 포함한 한 줄을 통째로 입력받은 뒤 int로 변환
 ### 요구사항 2 - get 방식으로 회원가입
 * 
 
@@ -38,26 +50,32 @@
 * > HTTP/1.1 302 Found  
     Location: /index.html 
 * 302 Found 리다이렉트 상태 응답 코드는 클라이언트가 요청한 리소스가 Location 헤더에 주어진 URL에 일시적으로 이동되었음을 가리킨다.
-* 302 Status code를 활용해서 페이지를 이동할 경우 요청과 응답이 한 번이 아니라 두 번 발생한다. 여기서는 
+* 302 Status code를 활용해서 페이지를 이동할 경우 요청과 응답이 한 번이 아니라 두 번 발생한다. 다음과 같은 과정이 일어난다. 
 ```
 /user/create 요청 -> 회원가입 처리 -> /index.html로 302 응답
 ```
-후
+다시 재요청
 ```
 /index.html 요청 -> /index.html 읽기 -> /index.html 200 응답
 ```
  > 302 Status code는 자바 웹개발에서 reponse.sendRedirect() 를 활용하면 똑같은 방식으로 작용하는데 내부적으로 302 Status code를 활용하는 것이다. (Location : ~~.~ 포함)  
  
  > 다른 웹 애플리케이션로 개발하면서 redirect 방식으로 페이지를 이동한다고 하면 내부적으로 302 Status code를 활용해서 이동하겠구나 하고 생각하면 됨.
+ 
 
 
 * <code>FileInputStream, FileOutputStream</code>은 **byte[] 단위의 데이터**만 입/출력을 할 수 있다. (문자나 byte 형식의 문자) 그래서 다른 데이터를 읽어야 할 때 항상 데이터를 변환해줘야 한다. 
 * <code>DataInputStream, DataOutputStream</code>은 자바 기본 자료형(char, int, long 등)으로 데이터를 입력하고 출력할 수 있다. 즉 데이터 입출력시 기본 자료형을 ByteStream으로 변환하여 파일입출력을 수행한다.
 ### 요구사항 5 - cookie
-* 
+* Login을 성공하면 Server에서 Header에 Set-Cookie : logined=true 응답값을 보낸다. (Chrome 개발자도구 Network Tab에서 Cookies/Response Cookies에 보면 값이 보임)
+* 다시 index.html로 재요청을 할 때 Request Cookies에 똑같이 logined=true를 담아서 다시 클라이언트에 보낸다. 이렇게 서버에서 클라이언트에게 Cookie 정보를 주면 브라우저는 Cookie를 인식을 해서 모든 요청에 대해서 Cookie를 담아서 서버에 보낸다.(css, favicon 등)
+* 즉 Server에서 응답을 할 때 Set-Cookie:logined=true Cookie값을 보내면 Browser는 이 Cookie 값을 읽은 다음에 이 Browser는 Server에 재요청을 할 때 Set-Cookie:logined=true가 포함되도록 처리가 된다.
 
 ### 요구사항 6 - stylesheet 적용
-* 
+* *.css 는 Content-type이 text/html 이 아니라 text/css라서 바꿔주기만 하면 됨
 
+* String 클래스의 <code>trim()</code>은 문자열에 있는 왼쪽, 오른쪽 공백을 다 제거 해준다.
+* <code>parseBoolean()</code> 객체 타입을 String -> Boolean 바꿔주는데 값이 null이 아니고 값이 "true" 인 경우에 boolean 타입으로 <code>true</code> 리턴해줌.(대/소문자 상관X)
+  > 같지 않을 경우 항상 <code>false</code>를 리턴함
 ### heroku 서버에 배포 후
 * 
--- Java/web-application-server/src/main/java/webserver/RequestHandler.java
+++ Java/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,12 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,9 +43,13 @@ public class RequestHandler extends Thread {
             if (isEmpty(line)) { return; }
             String url = HttpRequestUtils.getUrl(line);
             Map<String, String> headers = new HashMap<>();
+            boolean logined = false;
             while( !"".equals(line)){
                 log.debug("header : {}", line);
                 line = br.readLine();
+                if(line.contains("Cookie")){
+                    logined = isLogin(line);
+                }
                 String[] headerTokens = line.split(": ");
                 if(headerTokens.length == 2){
                     headers.put(headerTokens[0], headerTokens[1]);
@@ -57,19 +64,78 @@ public class RequestHandler extends Thread {
                 Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
                 User user = new User(params.get("userId"),params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User : {}", user);
+                DataBase.addUser(user);
 
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos);
+            } else if(url.equals("/user/login")) {
+                String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                log.debug("Request Body : {}", requestBody);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+                log.debug("UserId : {}, password : {}", params.get("userId"), params.get("password"));
+                User user = DataBase.findUserById(params.get("userId"));
+                if (user == null) {
+                    log.debug("User Not Found!");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302Header(dos);
+                } else if (params.get("password").equals(user.getPassword())) {
+                    log.debug("Login Success!");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302HeaderWithCookie(dos, "logined=true");
+                } else {
+                    log.debug("Password Mismatch!");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302Header(dos);
+                }
+            } else if (url.equals("/user/list")){
+                if(!logined){
+                    responseResource(out, "/user/login.html");
+                    return;
+                }
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users){
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos,body);
+            } else if (url.endsWith(".css")){
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200HeaderWithCss(dos, body.length);
+                responseBody(dos, body);
             }
 
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            responseResource(out, url);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void responseResource(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null){
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private boolean isEmpty(String line) {
@@ -89,11 +155,31 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+    private void response200HeaderWithCss(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
 
     private void response302Header(DataOutputStream dos) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Location: http://localhost:8080/index.html\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    private void response302HeaderWithCookie(DataOutputStream dos, String cookie) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: http://localhost:8080/index.html\r\n");
+            dos.writeBytes("Set-Cookie: " + cookie + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
