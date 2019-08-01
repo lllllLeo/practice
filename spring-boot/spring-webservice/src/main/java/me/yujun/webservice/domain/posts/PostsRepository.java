6532e6b2c7feb9bf3ee6e29043348b1d59e9bf76
lllllLeo <andwithyj@gmail.com>
2019-08-01 23:32:56 +0900

    spring boot webservice board

--- spring-boot/spring-webservice/src/main/java/me/yujun/webservice/domain/posts/PostsRepository.java
+++ spring-boot/spring-webservice/src/main/java/me/yujun/webservice/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package me.yujun.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
 
 /*
 * Mybatis 등에서 Dao라고 불리는 DB Layer 접근자이다. JPA에서는 Respository라고 부르며 인터페이스로 생성한다.
@@ -9,4 +12,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 * */
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p " +
+            "FROM Posts p " +
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
 }
--- spring-boot/spring-webservice/src/main/java/me/yujun/webservice/service/PostService.java
+++ spring-boot/spring-webservice/src/main/java/me/yujun/webservice/service/PostService.java
@@ -1,10 +1,14 @@
 package me.yujun.webservice.service;
 
 import lombok.AllArgsConstructor;
+import me.yujun.webservice.domain.dto.PostsMainResponseDto;
 import me.yujun.webservice.domain.dto.PostsSaveRequestDto;
 import me.yujun.webservice.domain.posts.PostsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  Controller에서 Dto.toEntity를 통해서 바로 전달해도 되지만 굳이 나눈 이유는
@@ -20,5 +24,17 @@ public class PostService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+
+    /*
+      .map(PostsMainResponseDto::new)는 실제로는 .map(posts -> new PostsMainResponseDto(posts))와 같습니다.
+      repository 결과로 PostsRepository에서 넘어온 Posts의 Stream을 map을 통해 PostsMainResponseDto로 변환 -> List로 반환하는 메소드
+    * */
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
