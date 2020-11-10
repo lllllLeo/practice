c07bcb3363b6a87bcd641cc5f0c62610c0be0ede
lllllLeo <andwithyj@gmail.com>
2020-11-10 14:23:29 +0900

    delete key in movie_crawler

--- python/movie_crawler/movie_crawler.py
+++ python/movie_crawler/movie_crawler.py
@@ -3,7 +3,7 @@ import telegram
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-bot = telegram.Bot(token= '1334951835:AAFr_xbwYYaSVRRffta0h7CUUnCMCCFo-uM')
+bot = telegram.Bot(token= '')
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20201103&screencodes=&screenratingcode=&regioncode='
 
 def job_function():
@@ -13,11 +13,11 @@ def job_function():
     if(imax):
         imax = imax.find_parent('div', class_='col-times')
         title = imax.select_one('div.info-movie > a > strong').text.strip()
-        bot.sendMessage(chat_id='1466067225', text=title + ' IMAX 예매가 열렸습니다.')
+        bot.sendMessage(chat_id='', text=title + ' IMAX 예매가 열렸습니다.')
     else:
         imax = imax.find_parent('div', class_='col-times')
         title = imax.select_one('div.info-movie > a > strong').text.strip()
-        bot.sendMessage(chat_id='1466067225', text=title + ' IMAX 예매가 아직 열리지 않았습니다.')
+        bot.sendMessage(chat_id='', text=title + ' IMAX 예매가 아직 열리지 않았습니다.')
 
 
 #
--- python/movie_crawler/telegram_bot.py
+++ python/movie_crawler/telegram_bot.py
@@ -1,9 +1,9 @@
 import telegram
 
-bot = telegram.Bot(token = '1334951835:AAFr_xbwYYaSVRRffta0h7CUUnCMCCFo-uM')
+bot = telegram.Bot(token = '')
 
 ## bot에 대한 업데이트 내용 가져오기
 # for i in bot.getUpdates():
 #     print(i.message)
 
-# bot.sendMessage(chat_id= '1466067225', text = '안녕ㅎㅎ')
+# bot.sendMessage(chat_id= '', text = '안녕ㅎㅎ')
